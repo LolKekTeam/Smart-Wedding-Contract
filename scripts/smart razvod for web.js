4a52627108d993bdc3a6cbec80de17c68962c7ed
Lol Kek Team <40458254+LolKekTeam@users.noreply.github.com>
2018-06-23 19:24:11 +0300

    Web UI

--- scripts/smart razvod for web.js
+++ scripts/smart razvod for web.js
@@ -5,21 +5,19 @@ contract brak {
     enum ActionChoices { setAddresses, send_h, send_w, send_t, razvodAuto, razvod50na50, razvodKoef }
     ActionChoices choice;
 
-    /*address own = 0x0B361c9Fd431fCaBE0A4D9d9F32E7680136C56d7;
-    address addOne = 0xE27623a11d5B67e2C4d54326eA5FB00f0eA20398;
-    address addTwo = 0x3d7992cFbBeeFaEdde4cFc44A68a93Dd0630709F;
-    address addThrid = 0x95Fa3CbF9D1e54226Be8Cb3373f2BDE5bA04A853;*/
-    uint p1 = 0; uint p2 = 0; uint p3 = 0; uint b1 = 0; uint b2 = 0; uint b3 = 0;
-    address addOne; address addTwo; address own; uint k;
-    function getRazvod() public constant returns(uint, uint, uint) {
-        return (b1, b2, b3);
-    }
-    function setAddresses(address _addOne, address _addTwo, address _own, uint _k) public
+    address own = 0x96ae57e6370ab3c547f91af35998d35d98a5e727;
+    address addOne = 0xa6d2f20f7f38e9cc9016430dcfa0752229118a55;
+    address addTwo = 0xaeadd10c6a168eeb02d24a1cb6d542a7c1903775;
+    /*address addThrid = 0x95Fa3CbF9D1e54226Be8Cb3373f2BDE5bA04A853;*/
+    uint p1 = 0; uint p2 = 0; uint p3 = 0; /*uint b1 = 0; uint b2 = 0; uint b3 = 0;
+    address addOne; address addTwo; address own;*/ uint k;
+
+    /*function setAddresses(address _addOne, address _addTwo, address _own, uint _k) public 
     {
         choice = ActionChoices.setAddresses;
         addOne = _addOne; addTwo = _addTwo; own = _own; k = _k;
-    }
-    mapping(address => uint) balances;
+    }*/
+    mapping(address => uint256) balances;
     function send_h(uint amount)public
     payable
     {
@@ -28,9 +26,6 @@ contract brak {
         p1 += amount;
         require(msg.value >= 10000000000000000);
         own.transfer(msg.value);
-        b1 = balances[addOne];
-        b2 = balances[addTwo];
-        b3 = balances[own];
     }
 
     function send_w(uint amount)public
@@ -41,9 +36,6 @@ contract brak {
         p2 += amount;
         require(msg.value >= 10000000000000000);
         own.transfer(msg.value);
-        b1 = balances[addOne];
-        b2 = balances[addTwo];
-        b3 = balances[own];
     }
 
     function send_t(uint amount)public
@@ -54,9 +46,6 @@ contract brak {
         p3 += amount;
         require(msg.value >= 10000000000000000);
         own.transfer(msg.value);
-        b1 = balances[addOne];
-        b2 = balances[addTwo];
-        b3 = balances[own];
     }
 
 
@@ -68,9 +57,6 @@ contract brak {
         addOne.transfer(((msg.value - p3) / (p1 + p2) * p1) + (p3 / 2));
         addTwo.transfer(((msg.value - p3) / (p1 + p2) * p2) + (p3 / 2));
         selfdestruct(own);
-        b1 = balances[addOne];
-        b2 = balances[addTwo];
-        b3 = balances[own];
     }
 
 
@@ -83,9 +69,6 @@ contract brak {
         addOne.transfer(msg.value / 2);
         addTwo.transfer(msg.value / 2);
         selfdestruct(own);
-        b1 = balances[addOne];
-        b2 = balances[addTwo];
-        b3 = balances[own];
     }
 
     function razvodKoef() public
@@ -98,9 +81,6 @@ contract brak {
         addOne.transfer(msg.value * k / 100);
         addTwo.transfer(msg.value * (100 - k) / 100);
         selfdestruct(own);
-        b1 = balances[addOne];
-        b2 = balances[addTwo];
-        b3 = balances[own];
     }
 
     function getChoice()public constant returns(ActionChoices)
@@ -118,5 +98,8 @@ contract brak {
     function getP3()public constant returns(uint)
     {
         return p3;
+    }
+    function getRazvod() public constant returns(uint256 b1, uint256 b2, uint256 b3) {
+        return (addOne.balance / 1000000000000000000, addTwo.balance / 1000000000000000000, own.balance / 1000000000000000000);
     }
 }
